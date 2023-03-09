2ab47bece98e043347c9b80748337506c48dfd27
Илья <ilion23082003@gmail.com>
2023-03-09 15:44:11 +0300

    add authorization

---
+++ AMONICWpf/AMONICWpf/Helper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+using System.Data;
+using System.Configuration;
+using System.Text;
+
+namespace AMONICWpf
+{
+
+    
+    internal class Helper
+    {
+        private static string connectionString = "Data Source=KEKS\\SQLEXPRESS;Initial Catalog=Session1_XX;Integrated Security=True";
+        public static SqlConnection connection = new SqlConnection(connectionString);
+       
+        public void create()
+        {
+            
+            
+        }
+
+
+
+
+
+        public static bool login(string email, string pass)
+        {
+            connection.Open();
+            SqlCommand logCommand = new SqlCommand("Select * from users where Email='"+ email+"' and Password='"+ returnHashPass(pass)+"';", connection);
+            SqlDataReader reader = logCommand.ExecuteReader();
+            bool result = reader.HasRows;
+            connection.Close();
+            return result;
+        }
+
+
+
+
+
+
+
+
+        public static string returnHashPass(string pass)
+        { 
+            MD5 md5 = MD5.Create();
+            var hash = Encoding.UTF8.GetBytes(pass);
+
+            return Convert.ToBase64String(md5.ComputeHash(hash));
+        }
+
+    }
+}
--- AMONICWpf/AMONICWpf/MainWindow.xaml.cs
+++ AMONICWpf/AMONICWpf/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -24,5 +25,132 @@ namespace AMONICWpf
         {
             InitializeComponent();
         }
+
+        bool acces = true;
+        int countertries = 0;
+        private void ExitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (!acces) return;
+
+            if(!Helper.login(Username.Text, Password.Password))
+            {
+                MessageLabel.Content = "Неверные логил или пароль";
+                countertries++;
+            }
+            else
+            {
+                MessageLabel.Content = "Успешная авторизация";
+            }
+            if (countertries == 3) updateTimer();
+        }
+
+        async Task updateTimer()
+        {
+            await Task.Run(async () =>
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    acces = false;
+                });
+
+                for (int i = 10; i > 0; i--)
+                {
+                    Dispatcher.Invoke(() =>
+                    {
+                        MessageLabel.Content = $"Вход заблокирован еще на {i} секунд";
+                    });
+                    await Task.Delay(1000);
+                }
+
+                Dispatcher.Invoke(() =>
+                {
+                    acces = true;
+                    countertries = 0;
+                    MessageLabel.Content = $"";
+                });
+            });
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //private void b1_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if (!acces)
+        //    {
+        //        l1.Content = "Доступ запрещен";
+        //        return;
+        //    }
+
+        //    if(t1.Text.Length>1 && t1.Text.Length < 20)
+        //    {
+        //        l1.Content = Helper.returnHashPass(t1.Text);
+        //    }
+        //    updateTimer();
+
+
+        //}
+
+        //public async Task updateTimer()
+        //{
+        //    await Task.Run(async() =>
+        //    {
+        //        Dispatcher.Invoke(() =>
+        //        {
+        //            acces = false;
+        //        });
+
+        //        for (int i = 5; i > 0; i--)
+        //        {
+        //            Dispatcher.Invoke(() =>{
+
+        //                l2.Content = $"Доступ запрещен на {i} секунд";
+        //            });
+        //            await Task.Delay(1000);
+        //        }
+
+        //        Dispatcher.Invoke(() =>
+        //        {
+        //            l2.Content = "";
+        //            acces = true;
+        //        });
+
+
+        //    });
+        //} 
+
     }
 }
+
+
